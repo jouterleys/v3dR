244928ddf92365dce9b374a02e744fa410285b9c
jo619 <jouterleys@bwh.harvard.edu>
2024-03-04 14:00:49 -0500

    Updating to deal with emtpy ASCIIs filling with NO_DATA

--- R/v3dR.R
+++ R/v3dR.R
@@ -10,44 +10,47 @@
 
 v3dR <- function(full_filepath){
 
-  df <- data.table::fread(file = full_filepath, header=FALSE, stringsAsFactors = F)
-  df <- data.table::setDF(df)
-  header <- df[c(1:5),c(2:ncol(df))]
-  data <- df[c(6:nrow(df)),c(2:ncol(df))]
-  item <- df[c(6:nrow(df)),1]
-  header <- t(header)
-  data <- t(data)
-  df <- cbind(header,data)
-  df <- data.table::as.data.table(df)
+  df <- data.table::fread(file = full_filepath, header = FALSE, stringsAsFactors = F)
 
+  if (length(df) < 2) {
+    # Create a data frame if txt is empty with NA values
+    df <- data.frame(c3d_name = "NO_DATA", signal_names = "NO_DATA", signal_types = "NO_DATA", signal_folder = "NO_DATA", signal_components = "NO_DATA", instance = NA, item = NA, value = NA
+    )
 
-  new_colNames <- c("c3d_name","signal_names","signal_types","signal_folder","signal_components",item)
-
-  data.table::setnames(df, colnames(df), new_colNames)
-
-  df <- tidyr::pivot_longer(as.data.frame(df), cols = -c("c3d_name","signal_names","signal_types","signal_folder","signal_components"),
-                            names_to = "item",
-                            values_to = "value")
-
-  if (is.factor(df$value)) {
-    df$value <- as.numeric(levels(df$value))[df$value]
   } else {
-    df$value <- as.numeric(df$value)
+
+    df <- data.table::setDF(df)
+
+    header <- df[c(1:5), c(2:ncol(df))]
+    data <- df[c(6:nrow(df)), c(2:ncol(df))]
+    item <- df[c(6:nrow(df)), 1]
+    header <- t(header)
+    data <- t(data)
+    df <- cbind(header, data)
+    df <- data.table::as.data.table(df)
+    new_colNames <- c("c3d_name", "signal_names", "signal_types", "signal_folder", "signal_components", item)
+
+    data.table::setnames(df, colnames(df), new_colNames)
+
+    df <- data.table::melt(df, id.vars = c("c3d_name", "signal_names", "signal_types", "signal_folder", "signal_components"), variable.name = "item", value.name = "value")
+
+    if (is.factor(df$value)) {
+      df$value <- as.numeric(levels(df$value))[df$value]
+    } else {
+      df$value <- as.numeric(df$value)
+    }
+
+    df$item <- factor(df$item)
+    df$item <- as.numeric(levels(df$item))[df$item]
+
+    df <- df %>% dplyr::group_by(dplyr::across(c(-value))) %>% dplyr::mutate(instance = 1:dplyr::n())
+
+    df <- df %>% dplyr::relocate(instance, .after = signal_components)
+
+    df <- as.data.frame(df)
+
+
   }
-
-  df$item <- factor(df$item)
-  df$item <- as.numeric(levels(df$item))[df$item]
-
-
-  df <- df %>%
-    dplyr::group_by(dplyr::across(c(-value))) %>%
-    dplyr::mutate(instance = 1:dplyr::n())
-
-  df <- df %>%
-    dplyr::relocate(instance, .after = signal_components)
-
-  df <-as.data.frame(df)
-
   return(df)
 
 }
